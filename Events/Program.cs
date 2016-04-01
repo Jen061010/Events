c357e946ab1d975563473cef8270e1181871445c
Sergi Sevil Montoro <Sergi Sevil Montoro>
2016-04-01 17:28:13 +0200

    Events C#

---
+++ Events/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Screen screen= new Screen();
+            botonEncendido_MyEvent boton= new botonEncendido_MyEvent;
+
+        }
+    }
+}
